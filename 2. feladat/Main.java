327bf62515d4e649c00131204c7c2fff2602a0c2
Hartyányi Bálint <39800003+Nevster92@users.noreply.github.com>
2024-05-28 20:13:29 +0200

    Add files via upload

---
+++ 2. feladat/Main.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args)  {
+
+        SecoundTask secoundTask = new SecoundTask();
+        secoundTask.resolveList(new int[]{1, 1, 2, 2, 2, 2, 2}, 2);
+}}
